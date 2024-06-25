5e34db03af861bfcdf2582227323563ded26dd1e
chronopio <joaquinalejandro00@gmail.com>
2024-06-25 23:25:36 +0200

    Update app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BotModule } from './bot/bot.module';
+import { WalletModule } from './blockchain/wallet/wallet.module';
+import { ProviderModule } from './blockchain/provider/provider.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), BotModule, WalletModule, ProviderModule],
   controllers: [AppController],
   providers: [AppService],
 })
