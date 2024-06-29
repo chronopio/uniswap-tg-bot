d923e25e1dee876a8a2bf0203f7557391bea3adf
chronopio <joaquinalejandro00@gmail.com>
2024-06-29 15:37:03 +0200

    Add and configure eslint and prettier

---
+++ server/eslint.config.mjs
@@ -0,0 +1,77 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const compat = new FlatCompat({
+  baseDirectory: dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:import/recommended",
+      "airbnb-base",
+      "airbnb-typescript/base",
+      "prettier",
+    ),
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    },
+
+    languageOptions: {
+      parser: tsParser,
+
+      parserOptions: {
+        project: "./tsconfig.build.json",
+        tsconfigRootDir: dirname,
+      },
+    },
+
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".ts", ".js"],
+        },
+        typescript: {},
+      },
+    },
+
+    rules: {
+      "consistent-return": "off",
+      "no-underscore-dangle": "off",
+      "arrow-body-style": "off",
+      "import/no-default-export": "error",
+      "import/prefer-default-export": "off",
+
+      "import/order": [
+        2,
+        {
+          alphabetize: {
+            order: "asc",
+          },
+
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        },
+      ],
+
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
+  {
+    ignores: ["**/contracts/**/*"],
+  },
+];
