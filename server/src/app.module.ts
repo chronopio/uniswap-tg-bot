dc8efa79d930e2c01d517aa02cafe7ae400361a8
chronopio <joaquinalejandro00@gmail.com>
2024-07-12 03:11:44 +0200

    Import telegraf in app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProviderModule } from './blockchain/provider/provider.module';
-import { WalletModule } from './blockchain/wallet/wallet.module';
-import { BotModule } from './bot/bot.module';
+import { TelegrafModule } from "nestjs-telegraf";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProviderModule } from "./blockchain/provider/provider.module";
+import { WalletModule } from "./blockchain/wallet/wallet.module";
+import { BotModule } from "./bot/bot.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), BotModule, WalletModule, ProviderModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        token: configService.get<string>("TELEGRAM_TOKEN"),
+      }),
+      inject: [ConfigService],
+    }),
+    BotModule,
+    WalletModule,
+    ProviderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
