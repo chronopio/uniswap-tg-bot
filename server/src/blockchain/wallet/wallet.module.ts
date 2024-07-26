4e181035bcf71d05d6a54fe91e771b762d9bdac4
chronopio <joaquinalejandro00@gmail.com>
2024-07-27 00:45:30 +0200

    Export wallet service

--- server/src/blockchain/wallet/wallet.module.ts
+++ server/src/blockchain/wallet/wallet.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 
-import { ProviderModule } from '../provider/provider.module';
-import { WalletService } from './wallet.service';
+import { ProviderModule } from "../provider/provider.module";
+import { WalletService } from "./wallet.service";
 
 @Module({
   imports: [ProviderModule],
   providers: [WalletService],
+  exports: [WalletService],
 })
 export class WalletModule {}
