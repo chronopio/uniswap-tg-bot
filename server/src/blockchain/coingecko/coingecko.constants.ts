2a1ddc255bd736b5b6188ce1ab6f3d7d324d559c
chronopio <joaquinalejandro00@gmail.com>
2024-07-27 00:46:16 +0200

    Add coingecko constants

---
+++ server/src/blockchain/coingecko/coingecko.constants.ts
@@ -0,0 +1,12 @@
+export const TokenSymbolToIds = {
+  BTC: "bitcoin",
+  ETH: "ethereum",
+  BNB: "binancecoin",
+  UNI: "uniswap",
+  LINK: "chainlink",
+  LTC: "litecoin",
+  BCH: "bitcoin-cash",
+  DOT: "polkadot",
+} as const;
+
+export const Currencies = ["BTC", "ETH", "BNB", "LINK", "LTC", "BCH", "DOT", "USD", "EUR"] as const;
