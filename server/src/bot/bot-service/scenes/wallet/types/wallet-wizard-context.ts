c803540b86e1ca45d8608496a94c372551fec34b
chronopio <joaquinalejandro00@gmail.com>
2024-08-11 21:24:52 +0200

    Add wallet wizard context type

---
+++ server/src/bot/bot-service/scenes/wallet/types/wallet-wizard-context.ts
@@ -0,0 +1,8 @@
+import { HDNodeWallet } from "ethers";
+import { Message, Update } from "telegraf/typings/core/types/typegram";
+import { WizardContext, WizardSession, WizardSessionData } from "telegraf/typings/scenes";
+
+export interface WalletWizardContext extends WizardContext<WizardSessionData> {
+  session: WizardSession<WizardSessionData> & { wallet: HDNodeWallet };
+  message: Update.New & Update.NonChannel & Message & { text: string };
+}
